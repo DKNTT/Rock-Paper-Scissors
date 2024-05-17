797da5dd0fb320a6d11b50e2c417c2521f5aa5a3
Dan <58478165+DKNTT@users.noreply.github.com>
2024-05-17 19:38:25 +0100

    add playRound function which decides who scores, and increments score counter

--- script.js
+++ script.js
@@ -23,9 +23,40 @@ function getHumanChoice() {
 
 function playRound(humanChoice, computerChoice) {
 
+    if ((humanChoice == 'rock') && (computerChoice == 'scissors')) {
+        console.log('You Win! Rock beats Scissors.');
+        humanScore++;
+    }
+    else if ((humanChoice == 'rock') && (computerChoice == 'paper')) {
+        console.log('You Lose! Paper beats Rock.');
+        computerScore++;
+    }
+    else if ((humanChoice == 'paper') && (computerChoice == 'rock')) {
+        console.log('You Win! Paper beats Rock.');
+        humanScore++;
+    }
+    else if ((humanChoice == 'paper') && (computerChoice == 'scissors')) {
+        console.log('You Lose! Scissors beats Paper.');
+        computerScore++;
+    }
+    else if ((humanChoice == 'scissors') && (computerChoice == 'rock')) {
+        console.log('You Lose! Rock beats Scissors.');
+        computerScore++;
+    }
+    else if ((humanChoice == 'scissors') && (computerChoice == 'paper')) {
+        console.log('You Win! Scissors beats Paper.');
+        humanScore++;
+    }
+    else {
+        console.log(`It\'s a draw! You both chose ${humanChoice}.`);
+    }
+
 }
 
 let humanScore = 0;
 let computerScore = 0;
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+
