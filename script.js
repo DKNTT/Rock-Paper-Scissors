6d231aff0b378351c1b13ce6d94cb55a504325b1
Dan <58478165+DKNTT@users.noreply.github.com>
2024-05-17 16:35:45 +0100

    add getComputerChoice, randomly returns rock paper or scissors

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello world!')
+function getComputerChoice() {
+    rand_num = Math.random()
+    if (rand_num < 1 / 3) {
+        return 'rock'
+    } else if ((rand_num >= 1 / 3) && (rand_num < 2 / 3)) {
+        return 'paper'
+    } else {
+        return 'scissors'
+    }
+}
+
+console.log(getComputerChoice())
